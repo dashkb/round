1249a2f1a04aef1f15d7b39be03b9d7bd8f54a42
Kyle Brett <kyle@kylebrett.com>
2013-09-21 14:01:20 -0700

    Add reimport/update

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,6 +1,4 @@
 class Source < ActiveRecord::Base
   has_many :tracks
-
-  validates :name, :root_path, presence: true
-  validates :name, :root_path, uniqueness: true
+  validates :name, :root_path, presence: true, uniqueness: true
 end
