00d70a7c9763202312065f4fb79d065a9e9c0979
Kyle Brett <kyle@kylebrett.com>
2013-10-13 22:30:52 -0400

    Global filter will return tracks, albums, or artists

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,11 @@
 class SearchController < ApplicationController
   def search
+    scope = Track.joins(:artist, :album)
+    where = "tracks.name ~* ? or artists.name ~* ? or albums.name ~* ?"
     regex = ".*#{params[:term]}.*"
+
     respond_with({
-      tracks: (Track.where "name ~* ?", regex)
+      tracks: scope.where(where, regex, regex, regex)
     })
   end
 end
